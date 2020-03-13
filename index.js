3be6bd192d709788eb627f1d3589752f3c4a0973
Misuti <h1710169@nushigh.edu.sg>
2020-03-13 19:33:08 +0800

    Heroku port fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const DEFAULT_GAME_SIZE = 10;
 const GAMES = {};
 
